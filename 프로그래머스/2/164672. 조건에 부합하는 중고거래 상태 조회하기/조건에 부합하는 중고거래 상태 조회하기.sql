893716dcb66ba84a054482d75f757bd9ee72a63d
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-01-26 17:42:20 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,8 @@
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
+        CASE WHEN status = 'DONE' THEN '거래완료'
+        WHEN status = 'SALE' THEN '판매중' 
+        WHEN status = 'RESERVED' THEN '예약중' 
+        END AS STATUS
+FROM USED_GOODS_BOARD
+WHERE CREATED_DATE = '2022-10-05'
+ORDER BY BOARD_ID DESC;
