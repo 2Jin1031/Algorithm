7264b680a705cc9fbdfc79c96651850c2cd80b40
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-02-14 15:56:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 224 ms, Memory: 10960 KB -BaekjoonHub

---
+++ 백준/Silver II/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,62 @@
+#include <string>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+static vector<vector<int> > A;
+static vector<int> visited;
+int DFS(int v, int order);
+
+int compare(int i, int j) {
+    return (i > j);
+}
+
+int main(void) {
+    int N, M, R; cin >> N >> M >> R;
+    A = vector<vector<int> >(N + 1);
+    visited = vector<int>(N + 1, 0);
+
+    // 초기화
+    for (int i = 1; i < N + 1; i++) {
+        A[i].push_back(i);
+    }
+
+    // Input
+    for (int i = 0; i < M; i++) {
+        int a, b; cin >> a >> b;
+        A[a].push_back(b);
+        A[b].push_back(a);
+    }
+
+    // Sort
+    for (int i = 1; i <= N; i++) {
+        if (A[i].size() >= 3) sort(A[i].begin() + 1, A[i].end(), compare);
+    }
+
+    // Main
+    int a = DFS(A[R].front(), 1);
+
+    // Print
+    for (int i = 1; i <= N; i++) {
+        cout << visited[i] << "\n";
+    }
+
+    return 0;
+}
+
+int DFS(int v, int order) {
+    if (visited[v]) {
+        return order;
+    }
+    visited[v] = order;
+
+    for (int i : A[v]) {
+        if (!visited[i]) {
+            order = DFS(i, ++order);
+        }
+    }
+    return order;
+}
