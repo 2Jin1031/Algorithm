97174b3ee72cc61050be4ed81a7d718f13293943
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-02-21 21:16:28 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 196 ms, Memory: 6148 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,60 +1,42 @@
 #include <string>
 #include <vector>
+#include <cstring>
 #include <iostream>
 #include <algorithm>
-#include <cstdio>
-#include <cstdlib>
+#include <functional>
+#include <queue>
+#include <cmath>
 
 using namespace std;
 
+static vector<vector<int> > A;
+static vector<bool> visited;
+
 int main(void) {
     int N, M; scanf("%d %d", &N, &M);
+    A = vector<vector<int> >(N + 1, vector<int> (N + 1, 0));
 
-    // Input
-    int **arr = (int **)malloc(sizeof(int *) * N); if (arr == NULL) return -1;
-    for (int i = 0; i < N; i++) {
-        arr[i] = (int *)malloc(sizeof(int) * N); if (arr[i] == NULL) return -1;
-        for (int j = 0; j < N; j++) {
-            scanf("%d", &arr[i][j]);
+    // Input A
+    int tmp;
+    for (int i = 1; i < N + 1; i++) {
+        for (int j = 1; j < N + 1; j++) {
+            scanf("%d", &A[i][j]);
         }
     }
 
-    // S
-    int **S = (int **)malloc(sizeof(int *) * N); if (S == NULL) return -1;
-    S[0] = (int *)malloc(sizeof(int) * N); if (S[0] == NULL) return -1;
-    S[0][0] = arr[0][0];
+    
 
-    for (int i = 1; i < N; i++) {
-        S[i] = (int *)malloc(sizeof(int) * N);
-        S[i][0] = S[i-1][0] + arr[i][0];
-    }
-    for (int j = 1; j < N; j++) {
-        S[0][j] = S[0][j-1] + arr[0][j];
-    }
-
-    for (int i = 1; i < N; i++) {
-        for (int j = 1; j < N; j++) {
-            S[i][j] = S[i-1][j] + S[i][j-1] - S[i-1][j-1] + arr[i][j];
+    // Sum Vector
+    for (int i = 1; i < N + 1; i++) {
+        for (int j = 1; j < N + 1; j++) {
+            A[i][j] = A[i][j] + A[i-1][j] + A[i][j-1] - A[i-1][j-1];
         }
     }
 
-    int x1, y1, x2, y2;
-    int result;
+    // Input M
+    int x1, x2, y1, y2;
     for (int i = 0; i < M; i++) {
         scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-        if (x1 == 1 && y1 == 1) {
-            result = S[x2-1][y2-1];
-        }
-        else if (x1 == 1 && y1 != 1) {
-            result = S[x2-1][y2-1] - S[x2-1][y1-2];
-        }
-        else if (x1 != 1 && y1 == 1) {
-            result = S[x2-1][y2-1] - S[x1-2][y2-1];
-        }
-        else {
-            result = S[x2-1][y2-1] - S[x1-2][y2-1] - S[x2-1][y1-2] + S[x1-2][y1-2];
-        }
-        printf("%d\n", result);
+        printf("%d\n", A[x2][y2] - A[x2][y1 - 1] - A[x1 - 1][y2] + A[x1-1][y1-1]);
     }
-
 }
