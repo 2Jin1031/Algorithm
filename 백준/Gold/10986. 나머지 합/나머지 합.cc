1622c828d90647f6e0e198455e692ce0fbc01811
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-02-22 00:40:41 +0900

    [Gold III] Title: 나머지 합, Time: 228 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,41 +1,38 @@
 #include <string>
 #include <vector>
+#include <cstring>
 #include <iostream>
 #include <algorithm>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
+#include <functional>
+#include <queue>
+#include <cmath>
 
 using namespace std;
 
+static vector<long> A;
+static vector<long> D;
+static vector<long> C;
+static vector<bool> visited;
+
 int main(void) {
     int N, M; scanf("%d %d", &N, &M);
+    A = vector<long> (N + 1, 0);
+    D = vector<long> (N + 1, 0);
 
-    // Input
-    long long *arr = new long long[N];
-    long long *S = new long long[N + 1];
-    long long *C = new long long[M]; 
-    memset(C, 0, sizeof(long long) * M);
-
-    long long cnt = 0;
-    S[0] = 0;
+    for (int i = 1; i < N + 1; i++) {
+        scanf("%ld", &A[i]);
+        D[i] = (D[i-1] + A[i]) % M;
+    }
     
-    for (int i = 0; i < N; i++) {
-        scanf("%lld", &arr[i]);
-        S[i + 1] = (S[i] + arr[i]) % M;
-        if (S[i + 1] == 0) cnt++;
-        C[S[i + 1]]++; 
+    C = vector<long> (M, 0);
+    for (int i = 1; i < N + 1; i++) {
+        C[D[i]]++;
     }
-
+    
+    long result = C[0];
     for (int i = 0; i < M; i++) {
-        if (C[i] > 1) {
-            cnt += C[i]*(C[i]-1)/2;
-        }
+        result += (C[i] * (C[i] - 1)) / 2;
     }
-    printf("%lld\n", cnt);
-
-    delete[] arr;
-    delete[] S;
-    delete[] C;
-    return 0;
+    printf("%ld", result);
+    
 }
