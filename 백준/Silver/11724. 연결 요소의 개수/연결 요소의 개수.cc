05428c6c599c6d35d16bbdbe1c355c610e22bf9a
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-02-26 16:11:06 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 128 ms, Memory: 6412 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -2,46 +2,50 @@
 #include <vector>
 #include <cstring>
 #include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <stack>
 
 using namespace std;
-
-static vector<vector<int>> A;
+static vector<vector<int> > A;
 static vector<bool> visited;
 void DFS(int v);
 
 int main(void) {
-    int N, M; cin >> N >> M;
-    A.resize(N + 1);
+    int N, M; scanf("%d %d", &N, &M);
+    A = vector<vector<int> > (N + 1);
     visited = vector<bool>(N + 1, false);
 
     for (int i = 0; i < M; i++) {
-        int s, e;
-        cin >> s >> e;
-        A[s].push_back(e);
-        A[e].push_back(s);
+        int a, b; scanf("%d %d", &a, &b);
+        A[a].push_back(b);
+        A[b].push_back(a);
     }
 
+    // DFS
     int count = 0;
-
     for (int i = 1; i < N + 1; i++) {
-        if (!visited[i]) {
-            count++;
+        if(!visited[i]) {
             DFS(i);
+            count++;
         }
     }
-
-    cout << count << "\n";
+    printf("%d", count);
+    
+    return 0;
 }
 
 void DFS(int v) {
     if (visited[v]) {
         return ;
     }
-
     visited[v] = true;
 
     for (int i : A[v]) {
-        if (visited[i] == false) {
+        if (!visited[i]) {
             DFS(i);
         }
     }
