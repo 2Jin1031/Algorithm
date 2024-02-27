7fb1ecd6b044d1ceadf62e1c8063db1cfc069240
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-02-27 15:01:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2324 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -5,82 +5,63 @@
 #include <algorithm>
 #include <functional>
 #include <queue>
+#include <cmath>
+#include <deque>
+#include <stack>
 
 using namespace std;
-
 static vector<vector<int> > A;
-static vector<int> visited;
-static bool arrive;
+static vector<bool> visited;
+static int N, M;
 
-void DFS(int v, int depth);
-void BFS(int v, int b);
+void DFS(int v);
+void BFS(int v);
 
 int main(void) {
-    int N, M, V; cin >> N >> M >> V;
+    int V; scanf("%d %d %d", &N, &M, &V);
+
     A = vector<vector<int> >(N + 1);
-    visited = vector<int>(N + 1);
+    visited = vector<bool> (N + 1, false);
 
-    for (int i = 0; i < N + 1; i++) {
-        A[i].push_back(i);
-    }
-
-    // Input
     for (int i = 0; i < M; i++) {
-        int a, b; cin >> a >> b;
+        int a, b; scanf("%d %d", &a, &b);
         A[a].push_back(b);
         A[b].push_back(a);
     }
 
-    // Sort
-    for (int i = 0; i < N + 1; i++) {
-        sort(A[i].begin() + 1, A[i].end());
+    for (int i = 1; i < N + 1; i++) {
+        sort(A[i].begin(), A[i].end());
     }
 
-    // Main
-    DFS(A[V].front(), 1);
-    // for (int i = 1; i < N + 1; i++) {
-    //     cout << visited[i] << " ";
-    // }
-    cout << "\n";
-
-    fill(visited.begin(), visited.end(), 0);
-
-    BFS(A[V].front(), 1);
-    // for (int i = 1; i < N + 1; i++) {
-    //     cout << visited[i] << " ";
-    // }
+    DFS(V); printf("\n");
+    fill(visited.begin(), visited.end(), false);
+    BFS(V);
 }
 
-void DFS(int v, int depth) {
-    if (visited[v]) {
-        
-    }
-    visited[v] = depth;
-    cout << v << " ";
+void DFS(int v) {
+    printf("%d ", v);
+    visited[v] = true;
 
     for (int i : A[v]) {
         if (!visited[i]) {
-            DFS(i, depth + 1);
+            DFS(i);
         }
     }
 }
 
-void BFS(int v, int b) {
+void BFS(int v) {
     queue<int> myQueue;
-    myQueue.push(v);
+    myQueue.push(v); visited[v] = true;
     
-    visited[v] = b;
-
     while (!myQueue.empty()) {
         int now = myQueue.front();
-        cout << now << " ";
         myQueue.pop();
+        printf("%d ", now);
         for (int i : A[now]) {
             if (!visited[i]) {
-                visited[i] = (++b);
                 myQueue.push(i);
+                visited[i] = true;
             }
         }
     }
 }
-
