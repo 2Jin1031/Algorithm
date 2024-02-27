9babfcf61a87ac7789d9442c5c6076244c609abc
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-02-27 14:24:48 +0900

    [Gold V] Title: ABCDE, Time: 36 ms, Memory: 2188 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.cc
+++ 백준/Gold/13023. ABCDE/ABCDE.cc
@@ -4,50 +4,59 @@
 #include <iostream>
 #include <algorithm>
 #include <functional>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <stack>
 
 using namespace std;
-
 static vector<vector<int> > A;
 static vector<bool> visited;
-static bool arrive;
+static int N, M;
+static int flag;
 
-void DFS(int v, int depth);
+void DFS(int v, int count);
 
 int main(void) {
-    int N, M; cin >> N >> M;
-    A = vector<vector<int> >(N);
-    visited = vector<bool>(N, false);
+    scanf("%d %d", &N, &M);
 
-    for(int i = 0; i < M; i++) {
-        int a, b; cin >> a >> b;
+    A = vector<vector<int> >(N);
+    visited = vector<bool> (N, false);
+
+    for (int i = 0; i < M; i++) {
+        int a, b; scanf("%d %d", &a, &b);
         A[a].push_back(b);
         A[b].push_back(a);
     }
 
-    for(int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) {
+        fill(visited.begin(), visited.end(), false);
+        flag = 0;
         DFS(i, 1);
-        if (arrive) {
-            cout << "1";
+        if (flag) {
+            printf("1");
             return 0;
         }
     }
 
-    cout << "0";
+    printf("0");
     return 0;
 }
 
-void DFS(int v, int depth) {
-    if (depth == 5 || arrive) {
-        arrive = true;
-        return;
+void DFS(int v, int count) {
+    if (count == 5 || flag == 1) {
+        flag = 1;
+        return ;
     }
 
     visited[v] = true;
 
     for (int i : A[v]) {
-        if (!visited[i])
-            DFS(i, depth + 1);
+        if (!visited[i]) {
+            DFS(i, count + 1);
+        }
+        
     }
+
     visited[v] = false;
 }
-
