41caacf200b0692ad0041ea413587eb9fb6100a4
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-03-08 17:16:38 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <stack>
+
+using namespace std;
+
+static int N;
+static vector<int> A;
+static int M;
+static vector<int> B;
+
+int main(void) {
+cout<<"       _.-;;-._\n";
+cout<<"'-..-'|   ||   |\n";
+cout<<"'-..-'|_.-;;-._|\n";
+cout<<"'-..-'|   ||   |\n";
+cout<<"'-..-'|_.-''-._|\n";
+}
