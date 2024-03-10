df11aec689d75d5e543ac66bb0e41a5e3422ad9e
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-03-10 23:27:10 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+    cout<<"#  # #### #### #  #"<<"\n";
+    cout<<"#### #  # #  # # #"<<"\n";
+    cout<<"#### #  # #  # # #"<<"\n";
+    cout<<"#  # #### #### #  #"<<"\n";
+}
