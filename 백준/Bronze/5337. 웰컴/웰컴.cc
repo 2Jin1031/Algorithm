a01f1d1f6da723ed67301a1ca530f0cf4687a4ad
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-03-21 23:48:36 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(void) {
+    printf(".  .   .\n"); 
+    printf("|  | _ | _. _ ._ _  _\n"); 
+    printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+}
