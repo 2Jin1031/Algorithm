e8f8e70aed71be4efe562847d79a3f811c09df13
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-03-26 23:52:45 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,17 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+    int a, b; scanf("%d %d", &a, &b);
+    double result = a * b * 0.5;
+    printf("%.1f", result);
+}
