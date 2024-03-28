c7b3b987c06181364dc6478d6463e4c5f01bd4de
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-03-28 23:43:06 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,17 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+    int n; scanf("%d", &n);
+    int result = n - 2018 + 72;
+    printf("%d", result);
+}
