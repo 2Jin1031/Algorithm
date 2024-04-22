b161c900b6577669c463877472e262c09b3e2bdb
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-04-22 14:39:05 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring> // Include for strcmp
+#include <queue>
+#include <functional>
+#include <algorithm>
+#include <vector>
+
+// 15963
+
+using namespace std;
+
+int main(void) {
+    long long N, M; cin >> N >> M;
+    if (N == M) cout << 1;
+    else cout << 0;
+}
