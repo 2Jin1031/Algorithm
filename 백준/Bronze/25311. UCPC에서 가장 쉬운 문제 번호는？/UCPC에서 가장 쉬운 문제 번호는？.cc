6c371da37f2067015c1c95c7bfda5726efdfa50b
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-04-23 09:04:20 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring> // Include for strcmp
+#include <queue>
+#include <functional>
+#include <algorithm>
+#include <vector>
+
+//
+
+using namespace std;
+
+int main(void) {
+    int N; cin >> N;
+    cout << "A";
+
+    return 0;
+}
