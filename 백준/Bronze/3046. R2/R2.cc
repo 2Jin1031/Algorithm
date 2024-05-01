d1104f65358abfbd62f1e2ab7158ab36f2d64c5e
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-05-01 23:21:59 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,21 @@
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <queue>
+#include <cmath>
+#include <deque>
+#include <stack>
+#include <tuple>
+#include <utility>
+#include <climits>
+
+
+// 3046
+using namespace std;
+
+int main(void) {
+    int n, m; cin >> n >> m;
+    cout << 2 * m - n;
+}
