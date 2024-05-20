db764b47c202d08691f8f09027fd7cc05896c5d5
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-05-20 23:17:25 +0900

    [Bronze IV] Title: Hello Judge, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main(void) {
+    int N; cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cout << "Hello World, Judge " << i << "!" << endl;
+    }
+}
