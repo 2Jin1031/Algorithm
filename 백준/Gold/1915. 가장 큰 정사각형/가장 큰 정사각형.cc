71c5e481d442ec5d26d6e69db1141239db481dff
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-05-26 15:08:44 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 44 ms, Memory: 5972 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <locale>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+static int N, M;
+static int D[1001][1001];
+
+int main(void) {
+    cin >> N >> M;
+    long max = 0;
+
+    for (int i = 0; i < N; i++) {
+        string mline; cin >> mline;
+
+        for (int j = 0; j < M; j++) {
+            D[i][j] = mline[j] - '0';
+
+            if (D[i][j] == 1 && j > 0 && i > 0) {
+                D[i][j] = min(D[i - 1][j - 1], min(D[i - 1][j], D[i][j - 1])) + D[i][j];
+            }
+            if (max < D[i][j]) {
+                max = D[i][j];
+            }
+        }
+    }
+    cout << max * max << endl;
+}
