963fce5b9129a2d3a6670a7e1e4b2f63f3f994a7
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-05-28 23:18:21 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring> // Include for strcmp
+#include <queue>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+    int a, b; cin >> a >> b;
+    cout << b + (b - a);
+}
