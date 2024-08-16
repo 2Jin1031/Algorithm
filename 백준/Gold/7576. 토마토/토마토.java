1536211b6bc716c08b679f8446f91cf88d8b7aaa
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-08-16 21:16:33 +0900

    [Gold V] Title: 토마토, Time: 608 ms, Memory: 127536 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    private static int N;
+    private static int M;
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static ArrayList<int[]> ripeTomatoes = new ArrayList<>();
+
+    private static final int D_SIZE = 4;
+    private static final int[] DX = {-1, 1, 0, 0};
+    private static final int[] DY = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        initialize(br);
+
+        bfs(ripeTomatoes);
+
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    bw.write("-1");
+                    br.close();
+                    bw.close();
+                    return;
+                }
+                max = Math.max(max, map[i][j]);
+            }
+        }
+
+        bw.write(String.valueOf(max - 1));
+
+        br.close();
+        bw.close();
+    }
+
+    private static void initialize(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
+                    ripeTomatoes.add(new int[]{i, j});
+                }
+                if (map[i][j] == -1) {
+                    visited[i][j] = true;
+                }
+            }
+        }
+    }
+
+    private static void bfs(ArrayList<int[]> ripeTomatoes) {
+        Queue<int[]> queue = new LinkedList<>();
+        initBfsStart(queue, ripeTomatoes);
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            exploreNeighbors(current, queue);
+        }
+    }
+
+    private static void exploreNeighbors(int[] current, Queue<int[]> queue) {
+        for (int i = 0; i < D_SIZE; i++) {
+            int nextX = current[0] + DX[i];
+            int nextY = current[1] + DY[i];
+
+            if (isValidCoordinate(nextX, nextY) && !visited[nextX][nextY]) {
+                visited[nextX][nextY] = true;
+                map[nextX][nextY] = map[current[0]][current[1]] + 1;
+                queue.add(new int[]{nextX, nextY});
+            }
+        }
+    }
+
+    private static boolean isValidCoordinate(int nextX, int nextY) {
+        return nextX >= 0 && nextY >= 0 && nextX < map.length && nextY < map[0].length;
+
+    }
+
+    private static void initBfsStart(Queue<int[]> queue, ArrayList<int[]> ripeTomatoes) {
+        for (int i = 0; i < ripeTomatoes.size(); i++) {
+            queue.add(ripeTomatoes.get(i));
+            visited[ripeTomatoes.get(i)[0]][ripeTomatoes.get(i)[1]] = true;
+        }
+    }
+}
