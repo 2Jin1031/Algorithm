04f6c22acf1bb15755bf09f39e121633aa67ebf1
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-09-06 15:32:19 +0900

    [Gold V] Title: LCS, Time: 148 ms, Memory: 18964 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        int[][] dp = new int[len1 + 1][len2 + 1];
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        bw.write(dp[len1][len2] + "\n");
+        bw.close();
+        br.close();
+    }
+}
