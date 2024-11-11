1d9ae4dbc1af452fede5c502bf8a8f34a14259cd
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2024-11-11 12:48:45 +0900

    [Silver II] Title: Oil Deposits, Time: 128 ms, Memory: 14880 KB -BaekjoonHub

---
+++ 백준/Silver/4677. Oil Deposits/Oil Deposits.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N, M;
+    private static Character[][] arr;
+    private static boolean[][] visited;
+
+
+    private static final int[] DX = {1, 0, 0, -1, 1, 1, -1, -1};
+    private static final int[] DY = {0, 1, -1, 0, 1, -1, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
+
+        while (true) {
+            initialization(br);
+
+            if (N == 0 && M == 0) {
+                break;
+            }
+
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (!visited[i][j]) {
+                        bfs(i, j);
+                        count++;
+                    }
+                }
+            }
+            bw.write(count + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void initialization(BufferedReader br) throws IOException {
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new Character[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = line.charAt(j);
+                if (arr[i][j] == '*'){
+                    visited[i][j] = true;
+                }
+            }
+        }
+    }
+
+    private static int bfs(int startX, int startY) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{startX, startY});
+        visited[startX][startY] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int currentX = current[0];
+            int currentY = current[1];
+
+            for (int i = 0; i < 8; i++) {
+                int nextX = currentX + DX[i];
+                int nextY = currentY + DY[i];
+
+                if (isValidCoordinate(nextX, nextY) && !visited[nextX][nextY]) {
+                    queue.offer(new int[]{nextX, nextY});
+                    visited[nextX][nextY] = true;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static boolean isValidCoordinate(int nextX, int nextY) {
+        return nextX >= 0 && nextY >= 0 && nextX < N && nextY < M;
+    }
+}
