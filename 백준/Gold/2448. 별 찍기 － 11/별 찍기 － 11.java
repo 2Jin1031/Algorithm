930d8d8690e674002ae1836500e7ccd1eea98828
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2025-01-08 21:20:36 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 460 ms, Memory: 145140 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static char[][] map;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        map = new char[N][2 * N - 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N - 1; j++) {
+                map[i][j] = ' ';
+            }
+        }
+
+        drawStar(0, N - 1, N);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 2 * N - 1; j++) {
+                sb.append(map[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void drawStar(int x, int y, int size) {
+        if (size == 3) {
+            map[x][y] = '*';
+            map[x + 1][y - 1] = '*';
+            map[x + 1][y + 1] = '*';
+            map[x + 2][y - 2] = '*';
+            map[x + 2][y - 1] = '*';
+            map[x + 2][y] = '*';
+            map[x + 2][y + 1] = '*';
+            map[x + 2][y + 2] = '*';
+            return;
+        }
+
+        int newSize = size / 2;
+        drawStar(x, y, newSize);
+        drawStar(x + newSize, y - newSize, newSize);
+        drawStar(x + newSize, y + newSize, newSize);
+    }
+}
