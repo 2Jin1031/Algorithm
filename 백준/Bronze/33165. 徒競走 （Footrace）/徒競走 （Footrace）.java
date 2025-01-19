3c05f76b4190c79e08d200d4e5e7ad68816e5d71
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2025-01-19 22:15:28 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(reader.readLine());
+        int num2 = Integer.parseInt(reader.readLine());
+
+        System.out.println(num1 * num2);
+    }
+}
