4122cae24f28c8f628096d22eeb6999faf7a576f
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2025-01-31 17:22:23 +0900

    [Gold III] Title: 앱, Time: 132 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] memory = new int[N];
+        int[] cost = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            memory[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int MAX_COST = 10000;
+        int[] dp = new int[MAX_COST + 1];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = MAX_COST; j >= cost[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - cost[i]] + memory[i]);
+            }
+        }
+
+        for (int j = 0; j <= MAX_COST; j++) {
+            if (dp[j] >= M) {
+                System.out.println(j);
+                return;
+            }
+        }
+    }
+}
