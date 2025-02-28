2d578863939c1897c30415161d416bb895dc5671
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2025-02-28 22:40:50 +0900

    [Bronze V] Title: Can you add this?, Time: 104 ms, Memory: 14040 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T;
+        T = Integer.parseInt(br.readLine());
+        
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            System.out.println(a + b);
+        }
+    }
+}
