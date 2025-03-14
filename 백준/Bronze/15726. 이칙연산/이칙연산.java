d0ec7d2828bcce42939f2f6b6a478ff43894423b
2Jin1031 <111180367+2Jin1031@users.noreply.github.com>
2025-03-14 09:30:49 +0900

    [Bronze IV] Title: 이칙연산, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/15726. 이칙연산/이칙연산.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Map;
+import java.lang.Math;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        double result = a * (double)((double)Math.max(b, c) / Math.min(b, c));
+        System.out.println((int)result);
+    }
+}
