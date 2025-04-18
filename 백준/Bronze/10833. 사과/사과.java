c3c52c139f1f25cde792b9474ee0e9f49399b176
kali <111180367+2Jin1031@users.noreply.github.com>
2025-04-18 22:29:33 +0900

    [Bronze III] Title: 사과, Time: 104 ms, Memory: 14376 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        int totalRes = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            totalRes += b % a;
+        }
+        
+        System.out.println(totalRes);
+    }
+}
