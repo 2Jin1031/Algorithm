74b00d793985a55d251f064c16eecf8b17a6a920
kali <111180367+2Jin1031@users.noreply.github.com>
2025-04-22 09:46:57 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final int HOUR = 0;
+    private static final int MINUTE = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = "";
+        while ((input = br.readLine()) != null) {
+            System.out.println(input);
+        }
+    }
+}
