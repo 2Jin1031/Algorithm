fa29fe156c256b3b1b862e52b422e7b5e2a1c25d
kali <111180367+2Jin1031@users.noreply.github.com>
2025-04-24 00:18:52 +0900

    [Bronze IV] Title: Final Price, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/28224. Final Price/Final Price.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += Integer.parseInt(br.readLine());
+        }
+        System.out.println(sum);
+    }
+}
