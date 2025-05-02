2c0c6d07dfba151ec9e711b9bb717239fed33801
kali <111180367+2Jin1031@users.noreply.github.com>
2025-05-02 22:48:44 +0900

    [Bronze III] Title: 경기 결과, Time: 524 ms, Memory: 205932 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5523. 경기 결과/경기 결과.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] ans = new int[2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a > b) ans[0]++;
+            else if (a < b) ans[1]++;
+        }
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
