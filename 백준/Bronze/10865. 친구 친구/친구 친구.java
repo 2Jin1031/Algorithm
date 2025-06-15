bd3fe8759febaffe322afe2e8891c2e0c62b95ba
kali <111180367+2Jin1031@users.noreply.github.com>
2025-06-15 23:05:57 +0900

    [Bronze III] Title: 친구 친구, Time: 988 ms, Memory: 199780 KB -BaekjoonHub

---
+++ 백준/Bronze/10865. 친구 친구/친구 친구.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            arr[a]++;
+            arr[b]++;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
