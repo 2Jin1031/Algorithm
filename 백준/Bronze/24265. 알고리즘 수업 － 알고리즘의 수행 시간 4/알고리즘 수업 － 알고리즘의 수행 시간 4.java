a2952e4df347be51fe35cbf0fa227c572d1bf3a9
kali <111180367+2Jin1031@users.noreply.github.com>
2025-07-03 22:53:40 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(N * (N-1L) / 2L);
+        System.out.println("2");
+    }
+}
