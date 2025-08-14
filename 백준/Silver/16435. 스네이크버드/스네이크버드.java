27f20b9879d6382446fa7c008b8b9585b4457d43
kali <111180367+2Jin1031@users.noreply.github.com>
2025-08-15 02:50:37 +0900

    [Silver V] Title: 스네이크버드, Time: 112 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            if (L >= arr[i]) {
+                L++;
+            }
+        }
+        System.out.println(L);
+    }
+}
