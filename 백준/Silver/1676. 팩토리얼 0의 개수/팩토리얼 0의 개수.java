e90f205f22400606b78c29434020e06a739896c3
kali <111180367+2Jin1031@users.noreply.github.com>
2025-08-20 05:03:21 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 100 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        for (int d = 5; d <= n; d *= 5) count += n / d;
+        System.out.println(count);
+    }
+}
