3a132bea00fd3c796ee2e6f59e894161fe9a402c
kali <111180367+2Jin1031@users.noreply.github.com>
2025-08-21 04:03:29 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        System.out.println((long) Math.sqrt(N)); 
+    }
+}
