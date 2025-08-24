bc6900af10a9d6d14e0c00ee2d7f3077c8cb8279
kali <111180367+2Jin1031@users.noreply.github.com>
2025-08-24 22:59:30 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 328 ms, Memory: 27740 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -1,85 +1,83 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
+import java.lang.*;
 
 public class Main {
-    
+
     private static int N, M;
-    private static Character[][] arr;
-    private static boolean[][] visited;
-
-    private static int[] startPosition;
-
-    private final static int[] DX = {0, 0, -1, 1};
-    private final static int[] DY = {-1, 1, 0, 0};
+    private static final int[] DX = {0, 0, 1, -1};
+    private static final int[] DY = {1, -1, 0, 0};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        initialization(br);
-        int result = bfs(startPosition[0], startPosition[1]);
-
-        bw.write(result == 0 ? "TT" : String.valueOf(result));
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    private static void initialization(BufferedReader br) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new Character[N][M];
-        visited = new boolean[N][M];
-        startPosition = new int[2];
+        char[][] arr = new char[N][M];
+        boolean[][] visited = new boolean[N][M];
 
+        int[] startPosition = new int[2];
+        int maxPeopleCount = 0;
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
+            String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = line.charAt(j);
+                arr[i][j] = input.charAt(j);
                 if (arr[i][j] == 'X') {
                     visited[i][j] = true;
+                    continue;
                 }
-                if (arr[i][j] == 'I') {
+                else if (arr[i][j] == 'I') {
                     startPosition[0] = i;
                     startPosition[1] = j;
+                    continue;
+                } else if (arr[i][j] == 'P') {
+                    maxPeopleCount++;
                 }
             }
         }
+
+        int ans = BFS(arr, visited, startPosition, maxPeopleCount);
+        if (ans == 0) {
+            bw.write("TT");
+        } else {
+            bw.write(String.valueOf(ans));
+        }
+        
+        bw.flush();
+        bw.close();
     }
 
-    private static int bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{x, y});
-        visited[x][y] = true;
+    private static int BFS(char[][] arr, boolean[][] visited, int[] startPosition, int maxPeopleCount) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(startPosition);
+        visited[startPosition[0]][startPosition[1]] = true;
 
-        int cntPerson = 0;
-        while (!queue.isEmpty()) {
+        int peopleCount = 0;
+        while (!queue.isEmpty() && peopleCount < maxPeopleCount) {
             int[] current = queue.poll();
             int currentX = current[0];
             int currentY = current[1];
 
-            if (arr[currentX][currentY] == 'P') {
-                cntPerson++;
-            }
             for (int i = 0; i < 4; i++) {
                 int nextX = currentX + DX[i];
                 int nextY = currentY + DY[i];
 
                 if (isValidCoordinate(nextX, nextY) && !visited[nextX][nextY]) {
-                    visited[nextX][nextY] = true;
                     queue.offer(new int[]{nextX, nextY});
+                    visited[nextX][nextY] = true;
+                    if (arr[nextX][nextY] == 'P') {
+                        peopleCount++;
+                    }
                 }
             }
         }
-
-        return cntPerson;
+        return peopleCount;
     }
 
     private static boolean isValidCoordinate(int nextX, int nextY) {
-        return nextX >= 0 && nextY >= 0 && nextX < N && nextY < M;
+        return nextX >= 0 && nextX < N && nextY >= 0 && nextY < M;
     }
 }
