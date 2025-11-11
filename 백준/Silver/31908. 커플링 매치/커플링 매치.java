d7618115f4e9faaa95c4ae0ed36e80a60e5fffca
kali <111180367+2Jin1031@users.noreply.github.com>
2025-11-11 21:35:53 +0900

    [Silver V] Title: 커플링 매치, Time: 168 ms, Memory: 16860 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/31908. 커플링 매치/커플링 매치.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        Map<String, List<String>> map = new HashMap<>();
+
+        StringTokenizer st;
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String key = st.nextToken();
+
+            List<String> names = map.getOrDefault(key, new ArrayList<>());
+            if (names.size() > 1 && !key.equals("-")) {
+                count--;
+            } else if (names.size() == 1 && !key.equals("-")) {
+                count++;   
+            }
+            names.add(name);
+            map.put(key, names);
+        }
+
+        System.out.println(count);
+        if (count == 0) return ;
+        for (String key : map.keySet()) {
+            List<String> names = map.get(key);
+            if (names.size() == 2 && !key.equals("-")) {
+                String ans = "";
+                for (int i = 0; i < names.size(); i++) {
+                    ans += names.get(i) + " ";
+                }
+                System.out.println(ans);
+            }
+        }
+    }
+}
