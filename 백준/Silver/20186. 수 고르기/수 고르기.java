d58024ec8a831759fa71a3627c9aa34b5d3349c1
kali <111180367+2Jin1031@users.noreply.github.com>
2025-12-28 23:07:12 +0900

    [Silver IV] Title: 수 고르기, Time: 136 ms, Memory: 15668 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20186. 수 고르기/수 고르기.java
+++ 백준/Silver/20186. 수 고르기/수 고르기.java
@@ -2,41 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    private static int N, K;
-    private static int[] arr;
-    
-    private static int ans = 0;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
+        int[] a = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 0; i < N; i++) a[i] = Integer.parseInt(st.nextToken());
 
-        backtracking(new boolean[N], 0, 0);
+        Arrays.sort(a);
+        long sumTopK = 0;
+        for (int i = N - 1; i >= N - K; i--) sumTopK += a[i];
+
+        long penalty = (long) K * (K - 1) / 2;
+        long ans = sumTopK - penalty;
+
         System.out.println(ans);
-    }
-
-    private static void backtracking(boolean[] visited, int count, int sum) {
-        if (count >= K) {
-            ans = Math.max(ans, sum);
-            return ;
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                backtracking(visited, count + 1, sum + arr[i] - count);
-                visited[i] = false;
-            }
-        }
     }
 }
