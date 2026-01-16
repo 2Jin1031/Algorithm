7de2d49224f111a875f497788f2aa7bdbb9a5f8e
kali <111180367+2Jin1031@users.noreply.github.com>
2026-01-17 01:27:33 +0900

    [Bronze V] Title: 콜센터, Time: 88 ms, Memory: 13780 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("     /~\\\n");
+        sb.append("    ( oo|\n");
+        sb.append("    _\\=/_" + "\n");
+        sb.append("   /  _  \\\n");
+        sb.append("  //|/.\\|\\\\\n");
+        sb.append(" ||  \\ /  ||\n");
+        sb.append("============\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+
+        System.out.print(sb.toString());
+    }
+}
