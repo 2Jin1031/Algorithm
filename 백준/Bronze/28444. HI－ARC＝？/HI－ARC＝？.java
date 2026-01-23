ec8995a56b96c2634e1560608832eef01bba6bc8
kali <111180367+2Jin1031@users.noreply.github.com>
2026-01-24 04:07:11 +0900

    [Bronze V] Title: HI-ARC=?, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int result = (H * I) - (A * R * C);
+        System.out.println(result);
+    }
+}
