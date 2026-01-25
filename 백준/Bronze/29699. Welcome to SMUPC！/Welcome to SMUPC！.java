e9658da5ab906f63eb35f0d949bb89b62afdf069
kali <111180367+2Jin1031@users.noreply.github.com>
2026-01-25 22:07:06 +0900

    [Bronze V] Title: Welcome to SMUPC!, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = "WelcomeToSMUPC";
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(str.charAt((N - 1) % str.length()) + "");
+    }
+}
