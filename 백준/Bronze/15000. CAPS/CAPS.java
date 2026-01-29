4e7d4b5334c8b87dba2005e2de61a059e6ae543a
kali <111180367+2Jin1031@users.noreply.github.com>
2026-01-30 00:19:08 +0900

    [Bronze V] Title: CAPS, Time: 288 ms, Memory: 21100 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.print(input.toUpperCase(Locale.ROOT));
+    }
+}
