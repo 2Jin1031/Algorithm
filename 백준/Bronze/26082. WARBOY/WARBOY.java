f378b7e01d4419b7dd7ddf73e5f1bb8347916410
kali <111180367+2Jin1031@users.noreply.github.com>
2026-02-03 01:24:10 +0900

    [Bronze V] Title: WARBOY, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        System.out.println(3 * B * C / A);
+    }
+}
