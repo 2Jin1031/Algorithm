98fc9f4716d547f870f4f345cdc7d3b038138213
kali <111180367+2Jin1031@users.noreply.github.com>
2026-02-16 00:50:27 +0900

    [Bronze V] Title: Rook, Time: 92 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String str = """
+  ___  ___  ___
+  | |__| |__| |
+  |           |
+   \\_________/
+    \\_______/
+     |     |
+     |     |
+     |     |
+     |     |
+     |_____|
+  __/       \\__
+ /             \\
+/_______________\\
+""";
+
+        System.out.print(str);
+    }
+}
