53fd75cf51f8ac3fffc017323d5c2ef491c9e739
kali <111180367+2Jin1031@users.noreply.github.com>
2026-02-18 02:19:59 +0900

    [Bronze V] Title: Арифметическая магия, Time: 112 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        System.out.print(1);
+    }
+}
