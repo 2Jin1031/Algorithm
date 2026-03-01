00ae3c4538adc9cfee8d30807bef6b817409ea84
kali <111180367+2Jin1031@users.noreply.github.com>
2026-03-02 05:11:23 +0900

    [Bronze V] Title: Correct, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int start = 9 * 60;
+        int submit = h * 60 + m;
+
+        System.out.println(submit - start);
+    }
+}
